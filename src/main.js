60f5d8b4ba8fd8aa5a0f2cdac5f5983c6a50fb79
ly286 <dlylove286@gmail.com>
2024-03-13 13:43:48 +0800

    首次提交

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//自定义全局css
+import '@/assets/css/global.css'
+
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+      port: 7070
+  },
+    //设置title
+    chainWebpack: config => {
+       config.plugin('html')
+           .tap(args => {
+               args[0].title = '农作物健康成长系统'
+               return args
+           })
+    }
 })
